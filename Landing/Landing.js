62662ed02c37a0574ff1f63d1984eb36ec617041
ponsru21 <vova.puzanov.05@mail.ru>
2025-05-28 19:55:32 +0800

    starting on js a bit, will continue tomorrow

---
+++ Landing/Landing.js
@@ -0,0 +1,15 @@
+(function() {
+    window.addEventListener('load', init);
+    function init() {
+        document.getElementById("start").addEventListener("click", startTest);
+        document.getElementById("results").addEventListener("click", showResults);
+    }
+
+    function startTest(){
+
+    }
+
+    function showResults(){
+        
+    }
+})()
